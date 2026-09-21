0d4d3ccfd005c9af8f484f04ea58832a2c726e89
S-Bhardwaj21 <ashrey.bhardwaj21>
2026-09-22 01:12:57 +0530

    Add Snowflake JWT authentication support

--- src/core/snowflake_client.py
+++ src/core/snowflake_client.py
@@ -3,12 +3,19 @@ import snowflake.connector
 from dotenv import load_dotenv
 load_dotenv()
 def get_connection():
-    return snowflake.connector.connect(
-        account=os.environ["SNOWFLAKE_ACCOUNT"],
-        user=os.environ["SNOWFLAKE_USER"],
-        authenticator=os.getenv("SNOWFLAKE_AUTHENTICATOR","externalbrowser"),
-        role=os.getenv("SNOWFLAKE_ROLE","ACCOUNTADMIN"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE","COMPUTE_WH"),
-        database=os.getenv("SNOWFLAKE_DATABASE","EXCEPTION_RESOLVER"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA","ENTERPRISE"),
-    )
+    authenticator=os.getenv("SNOWFLAKE_AUTHENTICATOR","externalbrowser")
+    connection_kwargs={
+        "account":os.environ["SNOWFLAKE_ACCOUNT"],
+        "user":os.environ["SNOWFLAKE_USER"],
+        "authenticator":authenticator,
+        "role":os.getenv("SNOWFLAKE_ROLE","ACCOUNTADMIN"),
+        "warehouse":os.getenv("SNOWFLAKE_WAREHOUSE","COMPUTE_WH"),
+        "database":os.getenv("SNOWFLAKE_DATABASE","EXCEPTION_RESOLVER"),
+        "schema":os.getenv("SNOWFLAKE_SCHEMA","ENTERPRISE"),
+    }
+    if authenticator=="SNOWFLAKE_JWT":
+        connection_kwargs["private_key_file"]=os.environ["SNOWFLAKE_PRIVATE_KEY_FILE"]
+        private_key_passphrase=os.getenv("SNOWFLAKE_PRIVATE_KEY_PASSPHRASE")
+        if private_key_passphrase:
+            connection_kwargs["private_key_file_pwd"]=private_key_passphrase
+    return snowflake.connector.connect(**connection_kwargs)
